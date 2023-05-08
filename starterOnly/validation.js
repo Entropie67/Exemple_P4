62bc33a9ed8b8dafc1d0bc788bab6401dd18de0b
Olivier <olivier.elophe@gmail.com>
2023-05-08 15:51:12 +0200

    structure de la validation globale.

--- starterOnly/validation.js
+++ starterOnly/validation.js
@@ -1,35 +1,48 @@
 
 
 let validation = () => {
-    // On commence par récupérer le champ prénom
-    let isValid = [];
-    isValid.push(valPrenom());
+    // Je lance l'ensemble des validations que je garde dans un tableau.
+    let isValid = [firstName(), lastName(), email(), birthdate(),  location(), checkbox()];
     // ......
     console.log(isValid);
-
-
+    // Vérifions que l'ensemble des champs sont bien à true.
     for (let i = 0; i < isValid.length; i++){
         if (!isValid[i]){
-            return false
+            console.log("L'un des champs n'est pas valide.")
+            return false;
         }
     }
-    return true
+    console.log("### L'ensemble du formulaire est valide ###")
+    return true;
 };
-
 // -------------------------------------------------
-
-let valPrenom = () => {
-    let prenom = document.getElementById("first").value;
-    let isValid = false;
-    if (prenom == "XX"){
-        isValid = true
-        // Je cache le message d'erreur
-    }
-    return isValid
+// Les fonctions de validation des différents champs
+// -------------------------------------------------
+let firstName = () => {
+    let first = document.getElementById("first").value;
+    return first.length > 2;
 }
 
+let lastName = () => {
+    // ...
+    return false;
+}
 
+let email = () => {
+    return false;
+}
 
+let birthdate = () => {
+    return false;
+}
+
+let location = () => {
+    return false;
+}
+
+let checkbox = () => {
+    return false;
+}
 //--------------------------------------------------
 let valid = document.getElementById("val");
 valid.addEventListener('click', (e) =>
